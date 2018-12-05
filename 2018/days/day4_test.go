c9b4bd221e34d14ea40131f1a6d16c80f8d387d5
Seth <402885+Setheck@users.noreply.github.com>
2018-12-05 00:07:51 -0800

    day4 setup

---
+++ 2018/days/day4_test.go
@@ -0,0 +1,16 @@
+package days_test
+
+import (
+	"testing"
+)
+
+const (
+	Day4Input       = "testdata/day4.txt"
+	Day4Part1Result = 0
+	Day4Part2Result = 0
+)
+
+func Test_Day4Part1(t *testing.T) {
+	// input := InputLines(t, Day4Input)
+
+}
